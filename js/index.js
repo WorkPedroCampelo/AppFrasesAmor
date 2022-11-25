d98ef8991ccf2fd1a60b97320176da301b990bff
WorkPedroCampelo <work.pedrocampelo@gmail.com>
2022-11-25 19:03:15 +0100

    UpdateDeFrases
    Hasta la frase número 30

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var numFrase = 1;
-var contadorFrases= 0;
+var contadorFrases = 0;
 var frase = "Prueba";
-var contadorFrasesTexto= "Número de frase= 0";
+var contadorFrasesTexto = "Número de frase= 0";
 
 
 
@@ -16,58 +16,227 @@ function contador() {
     var randomNum = randomNumber(1, 6);
     console.log(randomNum);
 
-    numFrase= randomNum;
+    numFrase = randomNum;
 
 
     switch (numFrase) {
         case 1:
             frase = "Hola mi amor, este es la primera frase, espero que te guste esta app <3";
             document.getElementById("fraseBonita").innerHTML = frase;
-            contadorFrases=1;
-            contadorFrasesTexto= "Número de frase = " + contadorFrases;
+            contadorFrases = 1;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
             document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
             break;
         case 2:
-            frase = "Hola cariño, un día más amandote, da igual que día leas esto ";
+            frase = "Hola cariño, un día más amandote, da igual cuando leas esto ";
             document.getElementById("fraseBonita").innerHTML = frase;
-            contadorFrases=2;
-            contadorFrasesTexto= "Número de frase = " + contadorFrases;
+            contadorFrases = 2;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
             document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
             break;
         case 3:
-            frase = "Hola que tal has pasado este dia? espero que muy bien, leuirgfh eroreuiy goeirb erg leyiru";
+            frase = "Dormiste bien hoy cariño? No soy adivino, dimelo por Whatsapp";
             document.getElementById("fraseBonita").innerHTML = frase;
-            contadorFrases=3;
-            contadorFrasesTexto= "Número de frase = " + contadorFrases;
+            contadorFrases = 3;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
             document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
             break;
         case 4:
-            frase = "Hola que tal has pasado este dia? espero que muy bien, leuirgfh eroreuiy goeirb erg leyiru";
+            frase = "Hay muchas cosas que no se de ti, pero sé que haces magia con solo sonreír y eso me basta";
             document.getElementById("fraseBonita").innerHTML = frase;
-            contadorFrases=4;
-            contadorFrasesTexto= "Número de frase = " + contadorFrases;
+            contadorFrases = 4;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
             document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
             break;
         case 5:
-            frase = "Hola que tal has pasado este dia? espero que muy bien, leuirgfh eroreuiy goeirb erg leyiru";
+            frase = "\"En los ojos correctos siempre serás arte\"";
             document.getElementById("fraseBonita").innerHTML = frase;
-            contadorFrases=5;
-            contadorFrasesTexto= "Número de frase = " + contadorFrases;
+            contadorFrases = 5;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
             document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
             break;
         case 6:
-            frase = "Hola que tal has pasado este dia? espero que muy bien, leuirgfh eroreuiy goeirb erg leyiru";
+            frase = "Mi lugar favorito del mundo es a tu lado";
             document.getElementById("fraseBonita").innerHTML = frase;
-            contadorFrases=6;
-            contadorFrasesTexto= "Número de frase = " + contadorFrases;
+            contadorFrases = 6;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
             document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
             break;
+        case 7:
+            frase = "Para cuando pillamos las maletas y nos vamos a Frankfurt un fin de semana?";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 7;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 8:
+            frase = "Te quiero y te querré, con tus defectos y tus virtudes";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 8;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 9:
+            frase = "Eres mi momento favorito del día.";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 9;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 10:
+            frase = "Sé que a tu lado siempre me sentiré como en casa";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 10;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 11:
+            frase = "No todos los días podrás con todo y no pasa nada solo tienes que seguir avanzando.";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 11;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 12:
+            frase = "No te habras vuelto a quedar dormida no, pedaso de vácula?";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 12;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 13:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 13;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 14:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 14;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 15:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 15;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 16:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 16;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 17:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 17;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 18:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 18;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 19:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 19;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 20:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 20;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 21:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 21;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 22:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 22;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 23:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 23;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 24:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 24;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 25:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 25;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 26:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 26;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 27:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 27;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 28:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 28;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 29:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 29;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+        case 30:
+            frase = "";
+            document.getElementById("fraseBonita").innerHTML = frase;
+            contadorFrases = 30;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
+            document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
+            break;
+
 
         default:
             frase = "Hola que tal has pasado este dia? espero que muy bien, leuirgfh eroreuiy goeirb erg leyiru";
             document.getElementById("fraseBonita").innerHTML = frase;
-            contadorFrases=0;
-            contadorFrasesTexto= "Número de frase = " + contadorFrases;
+            contadorFrases = 0;
+            contadorFrasesTexto = "Número de frase = " + contadorFrases;
             document.getElementById("contadorFrases").innerHTML = contadorFrasesTexto;
             break;
     }
